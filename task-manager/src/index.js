011ca5a8ee090104e40c8edf6402d0a3f63ebd26
Kevin Sparling <kevinsparling@gmail.com>
2019-07-08 16:27:17 +0800

    updated User and Task model, basic authentication in place

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/User')
-const Task = require('./models/Task')
 const userRouter = require('./routers/User')
 const taskRouter = require('./routers/Task')
 
@@ -12,7 +10,6 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-const router = new express.Router()
 
 app.listen(port, () => {
     console.log('server is up on port ' + port)
--- task-manager/src/routers/User.js
+++ task-manager/src/routers/User.js
@@ -37,8 +37,18 @@ router.post('/users', async (req, res) => {
         res.status(201).send(user)
     } catch (e) {
         res.status(400).send(e)
+        console.log(e)
     }
 
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch (e) {
+        res.status(400).send()
+    }
 })
 
 router.patch('/users/:id', async (req, res) => {
@@ -51,8 +61,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
         if (!user) {
             return res.status(404).send()
         }
