9da749a8aa6f712ed25c7224539eac0cba93735f
Kevin Sparling <kevinsparling@gmail.com>
2019-07-13 21:49:24 +0800

    added auth to tasks

--- task-manager/src/routers/Task.js
+++ task-manager/src/routers/Task.js
@@ -3,14 +3,6 @@ const router = new express.Router()
 const auth = require('../middleware/auth')
 const Task = require('../models/Task')
 
-router.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
 
 router.post('/tasks', auth, async (req, res) => {
     const task = new Task({
@@ -25,11 +17,20 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
+    try {
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: req.user._id })
 
         if (!task) {
             return res.status(404).send()
@@ -41,7 +42,7 @@ router.get('/tasks/:id', async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -51,8 +52,8 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try { 
-        //const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
-        task = await Task.findById(req.params.id)
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+
         updates.forEach((update) => task[update] = req.body[update])
         await task.save()
 
@@ -66,15 +67,15 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
 
         if (!task) {
             return res.status(404).send()
         }
 
-        res.send(user)
+        res.send(task)
     } catch (e) {
         res.status(500).send()
     }
