5b43a870c99c6fa42290ce1c97a2042ee105e083
Kevin Sparling <kevinsparling@gmail.com>
2019-07-23 17:26:04 +0800

    user avatars added, uploads but that needs to be fixed

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,10 +1,32 @@
 const express = require('express')
 require('./db/mongoose')
+const multer = require('multer')
 const userRouter = require('./routers/User')
 const taskRouter = require('./routers/Task')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+const upload = multer({
+    dest: 'uploads',
+    limits: {
+        fileSize: 10000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a word document'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
 
 // app.use((req, res, next) => {
 //     if (req.method === 'GET') {
@@ -27,6 +49,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('server is up on port ' + port)
 })
-
-const Task = require('./models/Task')
-const User = require('./models/User')
--- task-manager/src/routers/User.js
+++ task-manager/src/routers/User.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = new express.Router()
+const multer = require('multer')
 const User = require('../models/User')
 const auth = require('../middleware/auth')
+
 
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
@@ -71,6 +73,29 @@ router.post('/users/logoutAll', auth, async (req, res) => {
     }
 })
 
+// Setup Multer
+const upload = multer({
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter (req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG)$/)) {
+            return cb(new Error('please upload an image file'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
@@ -97,6 +122,12 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send()
     }
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
 })
 
 
